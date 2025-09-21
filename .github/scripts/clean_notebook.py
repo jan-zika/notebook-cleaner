95a53c927c94f173446598b1cf39afb9b63057f2
jan-zika <janzika@gmail.com>
2025-09-20 21:48:25 -0400

    Create clean_notebook.py

---
+++ .github/scripts/clean_notebook.py
@@ -0,0 +1,30 @@
+import nbformat
+import sys
+
+def clean_notebook(path_in, path_out=None):
+    nb = nbformat.read(path_in, as_version=nbformat.NO_CONVERT)
+
+    # Remove widget metadata if present
+    if "widgets" in nb.get("metadata", {}):
+        del nb["metadata"]["widgets"]
+
+    # Clear only widget outputs, keep normal outputs (plots, text)
+    for cell in nb.cells:
+        if "outputs" in cell:
+            cell["outputs"] = [
+                o for o in cell["outputs"]
+                if not (
+                    o.get("output_type") == "display_data"
+                    and "application/vnd.jupyter.widget-view+json" in o.get("data", {})
+                )
+            ]
+        if "execution_count" in cell:
+            cell["execution_count"] = None
+
+    nbformat.write(nb, path_out or path_in)
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: python clean_notebook.py notebook.ipynb [output.ipynb]")
+    else:
+        clean_notebook(sys.argv[1], sys.argv[2] if len(sys.argv) > 2 else None)
